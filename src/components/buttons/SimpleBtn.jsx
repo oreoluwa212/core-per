23b003f092c8b25e3f667e8a8d05210ac31f9714
oreoluwa212 <oreoluwaajayyiruth@gmail.com>
2024-09-25 05:58:24 +0100

    fix rendering

--- src/components/buttons/SimpleBtn.jsx
+++ src/components/buttons/SimpleBtn.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 function SimpleBtn({ href, className, children, onClick }) {
   return (
-    <a href={href}>
-      <button onClick={onClick} className={`bg-transparent text-white py-3 text-sm rounded-[24px] hover:bg-black font-medium ${className}`}
-    >
-      {children}
-    </button>
-    </a>
+    <Link to={href}>
+      <button
+        onClick={onClick}
+        className={`bg-transparent text-white py-3 text-sm rounded-[24px] hover:bg-black font-medium ${className}`}
+      >
+        {children}
+      </button>
+    </Link>
   );
 }
 
